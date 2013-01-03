538732efe0c0991fdd3e0d403f39a0c9c7fc2f24
Thomas Einwaller <thomas.einwaller@troii.com>
2013-01-03 21:29:36 +0100

    removed jquery again because of incompatibility

--- scrip.user.js
+++ scrip.user.js
@@ -5,31 +5,14 @@
 // ==/UserScript==
 
 (function() {
-	// a function that loads jQuery and calls a callback function when jQuery has finished loading
-	function addJQuery(callback) {
-		var script = document.createElement("script");
-		script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js");
-		script.addEventListener('load', function() {
-			var script = document.createElement("script");
-			script.textContent = "(" + callback.toString() + ")();";
-			document.body.appendChild(script);
-		}, false);
-		document.body.appendChild(script);
-	}
 
-	// the guts of this userscript
-	function main() {
-		var keyElement = $('#key-val');
-		var comment = keyElement.text() + ' - ' + $('#summary-val').text().trim();
+	var keyElement = document.getElementById('key-val');
+	var comment = keyElement.innerHTML + " " + document.getElementById('summary-val').innerHTML.trim();
 
-		var url = 'http://troii.com/zeroclipboard/ClipboardInterface.html'; // URL pointing to ClipboardInterface.html
-		var el = document.createElement("iframe");
-		el.src = url + "#" + escape(comment);
-		el.style.cssText = 'border: none; height: 22px; width: 20px; padding-right: 10px;';
-		keyElement.parent().after(el);
-	}
-
-	// load jQuery and execute the main function
-	addJQuery(main);
+	var url = 'http://troii.com/zeroclipboard/ClipboardInterface.html'; // URL pointing to ClipboardInterface.html
+	var el = document.createElement("iframe");
+	el.src = url + "#" + escape(comment);
+	el.style.cssText = 'border: none; height: 22px; width: 20px; padding-right: 10px;';
+	keyElement.parentNode.insertBefore(el, keyElement.nextSibling);
 
 })();

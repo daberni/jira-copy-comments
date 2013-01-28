3d571b024265c17af4760cc911f97197ffdc098d
Thomas Einwaller <thomas.einwaller@troii.com>
2013-01-28 11:28:41 +0100

    using innerText

--- scrip.user.js
+++ scrip.user.js
@@ -7,7 +7,13 @@
 (function() {
 
 	var keyElement = document.getElementById('key-val');
-	var comment = keyElement.innerHTML + " " + document.getElementById('summary-val').innerHTML.trim();
+	var comment = keyElement.innerHTML
+
+	if (document.body.innerText) {
+		comment += " " + document.getElementById('summary-val').innerText.trim();
+	} else {
+		comment += " " + document.getElementById('summary-val').innerHTML.replace(/\&lt;br\&gt;/gi,"\n").replace(/(&lt;([^&gt;]+)&gt;)/gi, "");
+	}
 
 	var url = 'http://troii.com/zeroclipboard/ClipboardInterface.html'; // URL pointing to ClipboardInterface.html
 	var el = document.createElement("iframe");

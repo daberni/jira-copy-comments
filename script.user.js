086dc3d17701f8134b10b62905e557f5a1a9a3ee
Bernhard Danecker <bernhard.danecker@gmail.com>
2017-08-14 09:03:58 +0200

    available on much more pages

--- script.user.js
+++ script.user.js
@@ -7,7 +7,7 @@
     textarea.remove();
 }
 
-function appendIcon(message, image) {
+function appendIcon(modifyFun, image) {
     var img = document.createElement("img");
     img.style.cssText = "position: relative; top: 3px;";
     img.src = image;
@@ -16,21 +16,56 @@ function appendIcon(message, image) {
     var anchor = document.createElement("a");
     anchor.href = "#";
     anchor.style.cssText = "padding-top: 9px;";
-    anchor.setAttribute("data-clipboard-text", message);
     anchor.appendChild(img);
-    anchor.addEventListener('click', function () {
-        if (document.queryCommandSupported("copy")) {
-            copyToClipboard(message);
-        } else {
-            alert(message);
+    anchor.addEventListener('click', function (event) {
+        var issueElement = document.getElementById('key-val');
+        if (!issueElement) {
+            issueElement = document.getElementById('issuekey-val');
         }
+        var message = "";
+        if (issueElement) {
+            var key = issueElement.innerText;
+            var message = document.getElementById('summary-val').innerText.trim();
+
+            if (modifyFun) {
+                message = modifyFun(key, message);
+            } else {
+                message = key + "-" + message;
+            }
+        } else {
+            issueElement = document.getElementsByClassName('issuerow focused')[0];
+            if (issueElement) {
+                var key = issueElement.getElementsByClassName('issuekey')[0].getElementsByTagName('a')[0].innerText;
+                var message = issueElement.getElementsByClassName('summary')[0].getElementsByTagName('a')[0].innerText;
+
+                if (modifyFun) {
+                    message = modifyFun(key, message);
+                } else {
+                    message = key + "-" + message;
+                }
+            }
+        }
+
+        anchor.setAttribute("data-clipboard-text", message);
+        if (message) {
+            if (document.queryCommandSupported("copy")) {
+                copyToClipboard(message);
+            } else {
+                alert(message);
+            }
+        }
+
+        event.preventDefault();
+        return false;
     });
 
-    var listitem = document.createElement("li");
-    listitem.appendChild(anchor);
-
     var createMenu = document.getElementById('create-menu');
-    createMenu.parentNode.appendChild(listitem);
+    if (createMenu) {
+        var listitem = document.createElement("li");
+        listitem.appendChild(anchor);
+
+        createMenu.parentNode.appendChild(listitem);
+    }
 }
 
 (function setIcons() {
@@ -39,32 +74,19 @@ function appendIcon(message, image) {
         elems[i].parentNode.parentNode.parentNode.removeChild(elems[i].parentNode.parentNode);
     };
 
-    var issueElement = document.getElementById('key-val');
-    if (!issueElement) {
-        return; // not on an issue page
-    }
-
-    var issue = issueElement.innerHTML;
-    var comment = issue;
-    if (document.body.innerText) {
-        comment += " " + document.getElementById('summary-val').innerText.trim();
-    } else {
-        comment += " " + document.getElementById('summary-val').innerHTML.replace(/\&lt;br\&gt;/gi, "\n").replace(/(&lt;([^&gt;]+)&gt;)/gi, "");
-    }
-    appendIcon(comment, "data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAUZJREFUeNqkU71KxEAQ3gPBQiuLvIJ5Ak2eIuns0qU4e4urUuYBBAOmCyKIVd5BiNfZWtsYCyFwYszuZt1v7i9eNlFwYJjdnW8+vpndnSil2H9sz3SYZdmVDtPVNgmC4HyIYAIFdzOXCj6OTtnX4TElwjCkmKYpxf3FMzt4nxPhWVxsCUFwe3GiPl+fVBzHqq5rVVWVKsuSHGucIQcMsKhZO7UgZcsWLw/Mtm0WRZFRquM4hAG2NwMhWtZq932ffMjeHi8J2yfgkgkp6SDP803Sdd3N2rIswgDbI+BcMMnbXtGuAQOsgUAzi2WiKApjsed5hOEmBaLZShtTQK02phYaodnloII1KTDAjrYwqmCoBQymqwAku0qWM5DmIaI3ubpfALvxxy1ojPEa8TjwwuY3s19/n/Eh6eLk/jqd/vEHJ93NtwADAFlI6xErpSp9AAAAAElFTkSuQmCC");
-
-    var name = issue;
-    if (document.body.innerText) {
-        name += "-" + document.getElementById('summary-val').innerText.trim();
-    } else {
-        name += "-" + document.getElementById('summary-val').innerHTML.replace(/\&lt;br\&gt;/gi, "\n").replace(/(&lt;([^&gt;]+)&gt;)/gi, "");
-    }
-    name = name.substring(0, issue.length + 25)
-        .replace('ä', 'ae')
-        .replace('ö', 'oe')
-        .replace('ü', 'ue')
-        .replace('ß', 'sz')
-        .replace('&', 'and')
-        .replace(/[^\w-]/g, '_');
-    appendIcon(name, "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAALiIAAC4iAari3ZIAAAAadEVYdFNvZnR3YXJlAFBhaW50Lk5FVCB2My41LjEwMPRyoQAAAPdJREFUOE+NkTEOAUEUhlett+EOGtxAnEKnV5JoVRKdA7iAM+g1iA0JDqBDRYOM7+3ui8nYYb/kk9038/+7ZgMfxpgKbnGNYTrOB4EiHlDZYb4SNsqTa9hHm/8lbJCwPPmCDezgDBV/CQsaVjbpXJRzUL5LGLhh4YZVlPWeDCw+JVxkhZUJtlHOZIAnVJISfuRT+ZCCLp6xjAWcorJy/5+LFgjN9I1HyW1MJIMQ5XWysAuuOMdnfGfMHst6DlklD2xhHe8ysPiEFQZuyRF1bRFPEr7DCgtuyRiH+IrvfoUVNvjO5H9YYaOU2J83f1ghUMIlRv5wELwB97Ju+zgKEekAAAAASUVORK5CYII=");
+    // Text
+    appendIcon(function (key, message) {
+        return key + " " + message;
+    }, "data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAUZJREFUeNqkU71KxEAQ3gPBQiuLvIJ5Ak2eIuns0qU4e4urUuYBBAOmCyKIVd5BiNfZWtsYCyFwYszuZt1v7i9eNlFwYJjdnW8+vpndnSil2H9sz3SYZdmVDtPVNgmC4HyIYAIFdzOXCj6OTtnX4TElwjCkmKYpxf3FMzt4nxPhWVxsCUFwe3GiPl+fVBzHqq5rVVWVKsuSHGucIQcMsKhZO7UgZcsWLw/Mtm0WRZFRquM4hAG2NwMhWtZq932ffMjeHi8J2yfgkgkp6SDP803Sdd3N2rIswgDbI+BcMMnbXtGuAQOsgUAzi2WiKApjsed5hOEmBaLZShtTQK02phYaodnloII1KTDAjrYwqmCoBQymqwAku0qWM5DmIaI3ubpfALvxxy1ojPEa8TjwwuY3s19/n/Eh6eLk/jqd/vEHJ93NtwADAFlI6xErpSp9AAAAAElFTkSuQmCC");
+    
+    // Branch name
+    appendIcon(function (key, message) {
+        return key + "-" + message.substring(0, 30)
+            .replace('ä', 'ae')
+            .replace('ö', 'oe')
+            .replace('ü', 'ue')
+            .replace('ß', 'sz')
+            .replace('&', 'and')
+            .replace(/[^\w-]/g, '_');
+    }, "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAALiIAAC4iAari3ZIAAAAadEVYdFNvZnR3YXJlAFBhaW50Lk5FVCB2My41LjEwMPRyoQAAAPdJREFUOE+NkTEOAUEUhlett+EOGtxAnEKnV5JoVRKdA7iAM+g1iA0JDqBDRYOM7+3ui8nYYb/kk9038/+7ZgMfxpgKbnGNYTrOB4EiHlDZYb4SNsqTa9hHm/8lbJCwPPmCDezgDBV/CQsaVjbpXJRzUL5LGLhh4YZVlPWeDCw+JVxkhZUJtlHOZIAnVJISfuRT+ZCCLp6xjAWcorJy/5+LFgjN9I1HyW1MJIMQ5XWysAuuOMdnfGfMHst6DlklD2xhHe8ysPiEFQZuyRF1bRFPEr7DCgtuyRiH+IrvfoUVNvjO5H9YYaOU2J83f1ghUMIlRv5wELwB97Ju+zgKEekAAAAASUVORK5CYII=");
 })();
